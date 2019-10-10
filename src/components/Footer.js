6a24d9a4a30467ee1a230953f76b0379ecfe35c1
David Kanter <david.kanter@herodigital.com>
2019-10-09 17:40:08 -0700

    Stopping for the day.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Footer = (props) => (
-    <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
-    </footer>
+const Footer = props => (
+  <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
+    <p className="copyright">
+      Built with <a href="http://www.reactjs.org">React</a> and{' '}
+      <a href="https://www.gatsbyjs.org/">Gatsby</a>. Hosted with{' '}
+      <a href="https://www.netlify.com">Netlify</a>. Template by{' '}
+      <a href="https://html5up.net">HTML5 UP</a>.
+    </p>
+    <p className="copyright">All photography &copy;2019 Dave Kanter</p>
+  </footer>
 )
 
 Footer.propTypes = {
-    timeout: PropTypes.bool
+  timeout: PropTypes.bool,
 }
 
 export default Footer
---
+++ src/components/MailLink.js
@@ -0,0 +1,17 @@
+const mailLink = () => {
+  let coded = 'i1BI@i1BIs1VDIk.Xcp'
+  let key = 'JNhMQHRZroBj1pGTn5WYPeUqA0bkc7SdzyLVgf4tI8wE6OsXuCiK2vlam3DFx9'
+  let shift = coded.length
+  let link = ''
+  for (let i = 0; i < coded.length; i++) {
+    if (key.indexOf(coded.charAt(i)) === -1) {
+      let ltr = coded.charAt(i)
+      link += ltr
+    } else {
+      let ltr = (key.indexOf(coded.charAt(i)) - shift + key.length) % key.length
+      link += key.charAt(ltr)
+    }
+  }
+  //return "<a href='mailto:" + link + "'>" + link + '</a>'
+  return link
+}
