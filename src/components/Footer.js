3321d20b3bd31285431890ad978be2ec6a2f1667
Dave Kanter <dave@lastrev.com>
2022-06-25 18:00:20 -0700

    Added target=_new to links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,23 @@ const year = new Date().getFullYear()
 const Footer = props => (
   <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
     <p className="copyright">
-      Built with <a href="http://www.reactjs.org">React</a> and{' '}
-      <a href="https://www.gatsbyjs.org/">Gatsby</a>. Hosted with{' '}
-      <a href="https://www.netlify.com">Netlify</a>. Template by{' '}
-      <a href="https://html5up.net">HTML5 UP</a>.
+      Built with{' '}
+      <a href="http://www.reactjs.org" target="_new">
+        React
+      </a>{' '}
+      and{' '}
+      <a href="https://www.gatsbyjs.org/" target="_new">
+        Gatsby
+      </a>
+      . Hosted with{' '}
+      <a href="https://www.netlify.com" target="_new">
+        Netlify
+      </a>
+      . Template by{' '}
+      <a href="https://html5up.net" target="_new">
+        HTML5 UP
+      </a>
+      .
     </p>
     <p className="copyright">All photos &copy;{year} Dave Kanter</p>
   </footer>
