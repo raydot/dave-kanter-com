adb0ee80e73a06be04ecb0d1441b339b18e70262
Dave Kanter <moadgoat@hotmail.com>
2024-07-01 15:25:44 -0700

    About to force fix npm.

--- src/pages/fino/fino.js
+++ src/pages/fino/fino.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
+function log() {
+  console.log('log')
+  console.log(getDate)
+}
+
+function getDate() {
+  return Date.now()
+}
+
 function fino() {
-  return <h1>Hello world!</h1>
+  return (
+    <form action={log}>
+      <label for="in_date">Date</label>
+      <input></input>
+      <label for="in_who">Who?</label>
+      <input></input>
+      <label for="in_amount">Amount</label>
+      <input></input>
+      <label for="in_category">Category</label>
+      <input></input>
+    </form>
+  )
 }
 
 export default fino
