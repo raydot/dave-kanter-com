2a88e2f354eb73b5b4d10c8ca5e28938194ff783
Dave Kanter <moatgoat@hotmail.com>
2026-04-10 10:14:24 -0700

    renamed 'middleware' to 'proxy.'

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { jwtVerify } from 'jose'
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   if (!request.nextUrl.pathname.startsWith('/admin')) {
     return NextResponse.next()
   }
